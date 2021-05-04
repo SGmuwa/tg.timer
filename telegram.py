90ba15a6570b383ea787c9d9d3202b79fe1dc862
Mikhail Pavlovich Sidorenko <m.sidorenko@kt-team.de>
2021-05-04 20:38:35 +0400

    Less code

--- telegram.py
+++ telegram.py
@@ -4,7 +4,6 @@ from re import sub
 from telethon import TelegramClient, events
 import telethon
 import asyncio
-import aiofiles
 from dataclasses import dataclass
 import subprocess
 from pathlib import Path
@@ -35,14 +34,7 @@ def get_good_users() -> list:
 good_users = get_good_users()
 
 
-@dataclass
-class Proc:
-    process: asyncio.subprocess.Process
-    stdin: aiofiles.open
-    stdout: aiofiles.open
-
-
-processes: dict[int, Proc] = dict()
+processes: dict[int, subprocess.Popen] = dict()
 
 with TelegramClient("check", get_api_id(), get_api_hash()) as client:
     client: TelegramClient = client
@@ -59,72 +51,62 @@ with TelegramClient("check", get_api_id(), get_api_hash()) as client:
             await client.send_message(user_id, l.decode())
             logger.trace("Sent.")
 
-    async def reader_from_exec(user_id: int, proc: Proc):
-        pid = proc.process.pid
-        while True:
-            logger.debug("start read")
-            bs = list()
-            while True:
-                logger.trace("Reading...")
-                bs.append(
-                    await asyncio.get_event_loop().run_in_executor(
-                        None,
-                        lambda: proc.process.stdout.read(1)
-                    )
+    async def reader_from_exec(user_id: int, proc: subprocess.Popen):
+        logger.debug("{} start read", user_id)
+        bs = list()
+        while not proc.stdout.closed:
+            logger.trace("{} Reading...", user_id)
+            bs.append(
+                await asyncio.get_event_loop().run_in_executor(
+                    None,
+                    lambda: proc.stdout.read(1)
                 )
-                logger.trace("Readed")
-                create_task(send_to_future(user_id, bs))
-            text = bs.decode()
-            if not text:
-                print("end read")
-                return
-            print("readed")
-            await client.send_message(user_id, text)
-            print("sendend")
+            )
+            logger.trace("{} Readed", user_id)
+            create_task(send_to_future(user_id, bs))
+        logger.debug("{} end read", user_id)
+    
+    async def good_user_handler(event: telethon.events.newmessage.NewMessage.Event):
+        message: telethon.tl.patched.Message = event.message
+        user_id = message.peer_id.user_id
+        if user_id not in processes:
+            process = subprocess.Popen(
+                "./checks.py",
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                close_fds=False
+            )
+            processes[user_id] = process
+            asyncio.create_task(
+                reader_from_exec(
+                    user_id,
+                    processes[user_id]
+                )
+            )
+        else:
+            if processes[user_id].stdin.closed:
+                processes[user_id].kill()
+                del processes[user_id]
+                await handler(event)
+            else:
+                logger.trace("Write data to checks.py...")
+                processes[user_id].stdin.write(message.message.encode() + b"\n")
+                processes[user_id].stdin.flush()
+                logger.trace("Write data ready.")
+                logger.debug("Return code: {}", processes[user_id].returncode)
 
     @client.on(events.NewMessage())
     async def handler(event: telethon.events.newmessage.NewMessage.Event):
-        message: telethon.tl.patched.Message = event.message
-        user_id = message.peer_id.user_id
-        logger.info("{}: {}", user_id, message.message)
-        await event.respond(str(user_id))
-        if user_id in good_users:
-            if user_id not in processes:
-                # Path(f"./{user_id}/").mkdir(parents=True, exist_ok=True)
-                # os.mkfifo(f"./{user_id}/in")
-                # os.mkfifo(f"./{user_id}/out")
-                # stdin = os.open(f"./{user_id}/in", os.O_WRONLY)
-                # if stdin < 1:
-                # 	raise EOFError(f"Can't open «./{user_id}/in»")
-                # stdout = os.open(f"./{user_id}/out", os.O_ASYNC & os.O_RDONLY)
-                # if stdout < 1:
-                # 	raise EOFError(f"Can't open «./{user_id}/out»")
-                process = subprocess.Popen(
-                    "./checks.py",
-					stdin=subprocess.PIPE,
-					stdout=subprocess.PIPE,
-					stderr=subprocess.STDOUT,
-					close_fds=False
-				)
-                processes[user_id] = Proc(
-                    process,
-                    stdin=10,
-                    stdout=12,
-                )
-                asyncio.create_task(reader_from_exec(
-                    user_id, processes[user_id]))
-            else:
-                # if processes[user_id].stdin.is_closing:
-                # 	processes[user_id].kill()
-                # 	del processes[user_id]
-                # 	await handler(event)
-                # else:
-                logger.trace("Write data to checks.py...")
-                processes[user_id].process.stdin.write(message.message.encode() + b"\n\0\-1")
-                processes[user_id].process.stdin.flush()
-                logger.trace("Write data ready.")
-                logger.debug("Return code: {}", processes[user_id].process.returncode)
-            # await event.respond(str(event))
+        try:
+            message: telethon.tl.patched.Message = event.message
+            user_id = message.peer_id.user_id
+            logger.info("{}: {}", user_id, message.message)
+            if user_id in good_users:
+                await good_user_handler(event)
+        except Exception as e:
+            logger.exception(e)
+            await client.send_message(user_id, str(e))
 
     logger.info("Telegram ready")
     client.run_until_disconnected()
