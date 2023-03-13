7d9480553e54b2595bc619f4640bc5ff6d600442
Mikhail Pavlovich Sidorenko <m.sidorenko@kt-team.de>
2023-03-13 08:34:16 +0400

    [master] Impl pizza

--- telegram.py
+++ telegram.py
@@ -10,14 +10,14 @@ except ModuleNotFoundError:
     install("telethon")
     from telethon import TelegramClient, events
 import telethon
-import asyncio
-import subprocess
+from datetime import datetime, timedelta
 try:
     from loguru import logger
 except ModuleNotFoundError:
     install("loguru")
     from loguru import logger
-from os import environ
+from os import environ, remove
+import re
 try:
     from json5 import load, loads
 except ModuleNotFoundError:
@@ -31,35 +31,39 @@ class Settings:
     def __init__(self, secret_path = environ.get("TELEGRAM_SECRET_PATH", "./secret.json5")):
         with open(secret_path) as f:
             self.json = load(f)
+        self._bot_user_id = re.sub(r":.*", "", self.json["bot_token"])
+        # remove(secret_path)
 
     @property
     def session_path(self) -> str:
-        return self.json["session_path"]
+        return self.json.pop("session_path")
 
     @property
     def api_id(self) -> int:
-        return self.json["api_id"]
+        return self.json.pop("api_id")
 
     @property
     def api_hash(self) -> str:
-        return self.json["api_hash"]
+        return self.json.pop("api_hash")
 
     @property
     def bot_token(self) -> str:
-        return self.json["bot_token"]
+        return self.json.pop("bot_token")
 
     @property
-    def white_list(self) -> list:
-        return self.json["white_list"]
-
+    def target_chat(self) -> int:
+        return self.json["target_chat"]
+    
     @property
-    def execute_app(self) -> list:
-        return loads(environ.get("TELEGRAM_EXECUTE_APP", '["python3", "./checks/__main__.py"]'))
+    def bot_user_id(self):
+        return self._bot_user_id
 
 
 settings = Settings()
 
 processes = dict()
+
+lastSend = None
 
 logger.trace("Init TelegramClient...")
 with TelegramClient(
@@ -74,87 +78,61 @@ with TelegramClient(
     def split_str_by_length(s: str, chunk_limit: int):
         return [s[i:i+chunk_limit] for i in range(0, len(s), chunk_limit)]
 
-    async def send_to_future(user_id, bs):
+    async def send_to_future(peer_id, msg, **kwargs):
         logger.trace("send_to_future sleep")
-        l = b''.join(bs)
-        bs.clear()
-        logger.trace("Ready to send {} KiB", len(l) / 1024)
-        if l:
-            logger.trace(f"get message from bytes length {len(l)}")
-            msg = l.decode()
+        logger.trace("Ready to send {} KiB", len(msg) / 1024)
+        if msg:
             logger.trace(f"ready chars {len(msg)}")
             msgs = split_str_by_length(msg, 4096)
             logger.trace(f"splitted! count: {len(msgs)}")
             logger.trace("Sending...")
             for m in msgs:
-                await client.send_message(user_id, m)
+                await client.send_message(peer_id, m, **kwargs)
             logger.trace("Sent.")
-
-    async def reader_from_exec(user_id: int, proc: subprocess.Popen):
-        logger.debug("{} start read", user_id)
-        bs = list()
-        while not proc.stdout.closed:
-            logger.trace("{}", bs[0] if bs else None)
-            task = asyncio.get_event_loop().call_later(
-                0.1, lambda: asyncio.get_running_loop().create_task(send_to_future(user_id, bs)))
-            to_append = await asyncio.get_event_loop().run_in_executor(
-                None,
-                lambda: proc.stdout.read(1)
-            )
-            if not to_append:
-                break
-            bs.append(to_append)
-            task.cancel()
-        await send_to_future(user_id, bs)
-        logger.debug("{} end read", user_id)
-
-    async def good_user_handler(event: telethon.events.newmessage.NewMessage.Event):
+    
+    async def getLinkOfMessage(event: telethon.events.newmessage.NewMessage.Event):
         message: telethon.tl.patched.Message = event.message
-        user_id = message.peer_id.user_id
-        if user_id not in processes:
-            process = subprocess.Popen(
-                settings.execute_app + [str(user_id)],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                close_fds=False
-            )
-            processes[user_id] = process
-            asyncio.create_task(
-                reader_from_exec(
-                    user_id,
-                    processes[user_id]
-                )
-            )
+        chat: telethon.types.Chat = await event.get_chat()
+        if chat.username:
+            return f"https://t.me/{chat.username}/{message.id}"
         else:
-            if processes[user_id].stdin.closed:
-                processes[user_id].kill()
-                del processes[user_id]
-                await handler(event)
-            else:
-                logger.trace("Write data to checks.py...")
-                processes[user_id].stdin.write(
-                    message.message.encode() + b"\n")
-                try:
-                    processes[user_id].stdin.flush()
-                except BrokenPipeError as e:
-                    processes[user_id].kill()
-                    del processes[user_id]
-                    await handler(event)
-                logger.trace("Write data ready.")
-                logger.debug("Return code: {}", processes[user_id].returncode)
+            return f"https://t.me/c/{chat.id}/{message.id}"
+        
+    async def buildAlertCallText(event: telethon.events.newmessage.NewMessage.Event):
+        message: telethon.tl.patched.Message = event.message
+        callText = re.sub(r"^/alert@ktpizzaechobot\s*", "", message.text)
+        link = await getLinkOfMessage(event)
+        output = f"На кухню зовёт @{(await message.get_sender()).username} в чате {link}"
+        if callText:
+            output += f": «{callText}»"
+        return output
+    
+    async def alert(event: telethon.events.newmessage.NewMessage.Event):
+        global lastSend
+        message: telethon.tl.patched.Message = event.message
+        if message.sender_id == settings.bot_user_id:
+            logger.warning(f"Sender is bot! Skip: {message.text}")
+            return
+        elif not message.text.startswith("/alert@ktpizzaechobot"):
+            raise Exception("Только команда «/alert@ktpizzaechobot» поддерживается из бесед.")
+        elif lastSend is not None and datetime.now() < lastSend + timedelta(minutes=30):
+            raise Exception(f"Слишком часто отправляете, подождите {lastSend + timedelta(minutes=30) - datetime.now()}.")
+        else:
+            lastSend = datetime.now()
+            await send_to_future(
+                telethon.types.PeerChannel(settings.target_chat),
+                await buildAlertCallText(event)
+            )
 
     @client.on(events.NewMessage())
     async def handler(event: telethon.events.newmessage.NewMessage.Event):
         try:
             message: telethon.tl.patched.Message = event.message
-            user_id = message.peer_id.user_id
-            logger.info("got message {}: {}", user_id, message.message)
-            if user_id in settings.white_list:
-                await good_user_handler(event)
+            logger.info("got message {}: {}", message.peer_id, message.message)
+            await alert(event)
         except Exception as e:
             logger.exception(e)
-            await client.send_message(user_id, str(e))
+            await send_to_future(message.peer_id, str(e), reply_to=event.message)
 
     logger.info("Telegram ready")
     client.run_until_disconnected()
